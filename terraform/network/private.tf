56f87c700eb37a924d0c737e227cd82871bcca05
alex75sapoz <alexei.sapoz@gmail.com>
2025-03-30 03:29:02 -0500

    Using github actions to trigger terraform cloud

--- terraform/network/private.tf
+++ terraform/network/private.tf
@@ -1,35 +1,16 @@
 resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.vpc.subnet.private.cidr
-  availability_zone = var.vpc.availability_zone_id
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.vpc.subnet.private.cidr
+  availability_zone       = var.vpc.availability_zone_id
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "private-${var.constants.environment.id}"
   }
 }
 
-resource "aws_eip" "main" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.main.id
-  subnet_id     = aws_subnet.private.id
-
-  tags = {
-    Name = "main-${var.constants.environment.id}"
-  }
-
-  depends_on = [aws_internet_gateway.main]
-}
-
 resource "aws_default_route_table" "private" {
   default_route_table_id = aws_vpc.main.default_route_table_id
-
-  route {
-    nat_gateway_id = aws_nat_gateway.main.id
-    cidr_block     = "0.0.0.0/0"
-  }
 
   tags = {
     Name = "private-${var.constants.environment.id}"
